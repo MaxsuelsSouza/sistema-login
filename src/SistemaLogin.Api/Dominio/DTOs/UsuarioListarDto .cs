7b9343b7bb715feda43dc3f550fa6334b5bade77
MaxsuelsSouza <maxsuel.souza@equipeblue.com.br>
2024-08-12 21:02:29 -0300

    teste

--- src/SistemaLogin.Api/Dominio/DTOs/UsuarioListarDto .cs
+++ src/SistemaLogin.Api/Dominio/DTOs/UsuarioListarDto .cs
@@ -5,7 +5,6 @@ namespace SistemaLogin.Api.Dominio.DTOs;
 [AutoMap(typeof(Usuario), ReverseMap = true)]
 public class UsuarioListarDto
 {
-    public Guid UsuarioId { get; set; }
     public string? NomeUsuario { get; set; }
     public string? Email { get; set; }
 }
